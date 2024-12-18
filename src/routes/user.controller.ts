e8e6cc263393bd51902b57ed244fb07df3154c08
= <=>
2024-12-18 15:07:06 +0330

    create user api

--- src/routes/user.controller.ts
+++ src/routes/user.controller.ts
@@ -1,9 +1,29 @@
 import { Request, Response, Router } from "express";
+import User from "../model/user/user.model";
 
 const userRouter = Router();
 
 userRouter.post("/users", async (req: Request, res: Response) => {
   const {email , password, username} = req.body;
+
+  if(!email || !password || username) {
+    res.status(400).send({message: "All information are required"})
+  }
+
+  try {
+    const existingUser = await User.find({email});
+    if(existingUser) {
+      res.status(409).send({message: "this email is taken"})
+      return;
+    }
+
+    const user = await User.create({email, password, username})
+    res.status(200).send({message: "user created!", user})
+
+  } catch(err) {
+    res.status(500).send({message: "Internal server error"})
+  }
+
 })
 
 export default userRouter;
