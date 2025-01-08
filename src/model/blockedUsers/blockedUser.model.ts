759c707f2ca4dbab85581c3a4556d2e935879cb3
= <=>
2025-01-08 14:57:06 +0330

    created BlockedUser model

---
+++ src/model/blockedUsers/blockedUser.model.ts
@@ -0,0 +1,18 @@
+import mongoose, { model } from "mongoose";
+import { IBlockedUsers } from "./blockedUser.type";
+
+const blockedUserSchema = new mongoose.Schema<IBlockedUsers>({
+  blockedId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  blockerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  }
+});
+
+const BlockedUser = model<IBlockedUsers>("BlockedUser", blockedUserSchema);
+export default BlockedUser;
