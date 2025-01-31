49306863421ba7928524598cf40842c8042f6b76
Mohammad <mohammadkeshavarz.a.13@gmail.com>
2025-01-31 23:02:28 +0330

    add chat api path

--- src/routes/chat.controller.ts
+++ src/routes/chat.controller.ts
@@ -2,27 +2,48 @@ import { Response, Router } from "express";
 import { RequestWithPayload } from "./user.controller";
 import User from "../model/user/user.model";
 import { authMiddleware } from "../middleware/auth.middleware";
+import Message from "../model/message/message.model";
 
 const chatRouter = Router();
 
-chatRouter.get("/chats", authMiddleware, async (req: RequestWithPayload, res: Response) => {
-      try {
-            const users = await User.find();
-            const filteredUsers = users.filter(user => user._id.toString() !== req.userId);
-            res.status(200).send(filteredUsers)
-      } catch(err) {
-            console.log("/chat", err);
-            res.status(500).send({message: "Internal server error"})
-      }
-});
+chatRouter.get(
+    "/chat-messages",
+    authMiddleware,
+    async (req: RequestWithPayload, res: Response) => {
+        const { friendId } = req.query as { friendId: string };
+        const userId = req.userId;
 
-chatRouter.get("/messages", authMiddleware, async (req: RequestWithPayload, res: Response) => {
-      try {
-            
-      } catch(err) {
-            console.log("/chat/messages", err);
-            res.status(500).send({message: "Internal Server Error"})
-      }
-})
+        if (!friendId) {
+            res.status(400).send("body is incorrect");
+            return;
+        }
+
+        try {
+            const messages = await Message.find({
+                $or: [
+                    { senderId: userId, recieverId: friendId },
+                    { senderId: friendId, recieverId: userId },
+                ],
+            }).sort({ createdAt: 1 });
+
+            console.log("userId", userId);
+            console.log("friendId", friendId);
+            // console.log(messages)
+            const sortedMessages = messages.map((message) => {
+                console.log(String(message.senderId) === userId);
+                return {
+                    id: message._id.toString(),
+                    text: message.message,
+                    type: String(message.senderId) === userId ? "out_box" : "in_box",
+                };
+            });
+
+            res.status(200).send(sortedMessages);
+        } catch (err) {
+            console.log("chat-messages -- get", err);
+            res.status(500).send({ message: "Internal server error" });
+        }
+    }
+);
 
 export default chatRouter;
